d8da571e986862acceb59e7e7d9dd91e7486896b
A F <fimine@gmail.com>
2017-01-25 11:02:15 -0500

    www

--- agfc-app/src/app/contactus/contact-form.service.ts
+++ agfc-app/src/app/contactus/contact-form.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Http, Response} from "@angular/http";
+import { Observable } from "rxjs/Observable";
+import { ContactForm }  from "contactus";
 
 @Injectable()
 export class SendContactFormService {
 private _apiUrl: string = "api.com";
     constructor(private _http: Http) { }
+
+postContactForm(contactForm : ContactForm) : Observable<number>{
+    
 }
+
+
+}
+
---
+++ agfc-app/src/app/contactus/contact-form.ts
@@ -0,0 +1,7 @@
+export class ContactForm {
+    public ContactFormId : number;
+    public Name : string;
+    public Email: string;
+    public Comments : string;
+    public Url : string;
+}
